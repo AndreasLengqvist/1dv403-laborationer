c0e9e1fa8d68fd083876ecca8d591b602c943e34
Andreas Lengqvist <al223bn@student.lnu.se>
2013-11-15 09:31:06 -0500

    Klar med Uppgift 1.

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,21 +2,33 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	
+    var secret = Math.floor( Math.random() * 100)+1;    // Slumpat hemligt tal.
+    var guesses = 0;    // Räknare som håller koll på antalet gissningar.
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
+    	    
+    	    guesses += 1;
+    	    
+    	    if (number > 100 || number < 1){
+    	        return [false, "Talet är utanför intervallet 0 - 100"];
+    	    }
+		
+    		if (number == secret){
+    		    return [true, "Grattis du vann! Det hemliga talet var "+ secret +" och du behövde "+ guesses +" gissningar för att hitta det."];
+    		}
+    		
+    	    if (number < secret){
+    	        return [false, "Det hemliga talet är lägre!"];
+    	    }
+    	    
+    	    if (number > secret){
+    	        return [false, "Det hemliga talet är högre!"];
+    	    }
 
-
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
 	};
 	
 	// ------------------------------------------------------------------------------
