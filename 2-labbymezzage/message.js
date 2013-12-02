d0b6b6048c83290788e72203879d8c55437d8375
Andreas Lengqvist <al223bn@student.lnu.se>
2013-12-02 10:54:39 -0500

    Klar med Moment 1. Kom igång - Skalet och strukturen.

---
+++ 2-labbymezzage/message.js
@@ -0,0 +1,43 @@
+"use strict";
+
+
+
+// Meddelande-konstruktor som tar text och datum som parametrar.
+function Message(message, date){
+    
+    // Hämtar meddelandetexten.
+    this.getText = function() {
+        return message;
+    }
+
+    // Sätter meddelandetexten.
+    this.setText = function(_text) {
+        message = _text;
+    }
+    
+    // Hämtar datumet.
+    this.getDate = function() {
+        return date;    
+    }
+    
+    // Sätter datumet.
+    this.setDate = function(_date) {
+        date = _date;
+    }
+};
+
+
+// toString-metod. En sträng presentation av objektet.
+Message.prototype.toString = function() {
+
+}
+
+// Hämtar texten med \n utbytt mot <br/>.
+Message.prototype.getHTMLText = function() {
+    
+}
+
+
+Message.prototype.getDateText = function() {
+    
+}
---
+++ 2-labbymezzage/messageboard.js
@@ -0,0 +1,12 @@
+"use strict";
+
+    var MessageBoard = {
+
+        init:function () {
+            alert("Test!")
+        
+        }
+};
+
+
+window.onload = MessageBoard.init;
