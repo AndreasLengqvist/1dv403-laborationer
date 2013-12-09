3b1777523ca3e2f7ca544cc918ef87744b4adbfd
Andreas Lengqvist <al223bn@student.lnu.se>
2013-12-09 10:23:39 -0500

    Påbörjat labb 3

---
+++ 3-gameon/memory/script/memory.js
@@ -0,0 +1,53 @@
+"use strict";
+
+    var Memory = {
+
+
+        memory: [],
+
+        init:function () {
+            var mem1 = new RandomGenerator.getPictureArray(4,4);
+            Memory.createTable();
+            console.log(mem1);
+        },
+        
+        createTable: function() {
+            var wrapper = document.querySelector("#memorygame");
+            var div = document.createElement("div");
+            wrapper.className = "wrapper";
+            wrapper.appendChild(div);
+
+            // Skapar en tabell.
+            var table = document.createElement("table");
+            table.className = "memtable";
+            div.appendChild(table);
+
+            // Lägger till arrayen (med tillhörande bilder) till tabellen med hjälp av for-loopar.
+            for (var rows = 0; rows < 4; rows += 1) {
+                var tr = document.createElement("tr");
+                table.appendChild(tr);
+                
+                for (var cols = 0; cols < 4; cols += 1) {
+                    var td = document.createElement("td");
+                    tr.appendChild(td);
+                    
+                    var pic = document.createElement("img");
+                    pic.setAttribute("src", "../pics/0.png");
+                    td.appendChild(pic);
+                    console.log(pic);
+                }
+            
+            }
+        },
+        
+        start: function() {
+            
+            
+            
+            
+        }
+            
+};
+
+    // MessageBoard.init anropas när sidan är helt färdigladdad.
+    window.onload = Memory.init;
