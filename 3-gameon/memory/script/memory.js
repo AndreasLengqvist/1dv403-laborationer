4712a6e59e0c9b9d154d400364b52c5da7635426
Andreas Lengqvist <al223bn@student.lnu.se>
2013-12-10 14:01:25 -0500

    Färdig med visning av symboler.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -3,19 +3,27 @@
     var Memory = {
 
 
-        memory: [],
+        memoryArray: [],
+        rows: 4,
+        cols: 4,
+
 
         init:function () {
-            var mem1 = new RandomGenerator.getPictureArray(4,4);
+            var memory1 = new RandomGenerator.getPictureArray(Memory.rows, Memory.cols);
+            Memory.memoryArray = memory1;
+            
             Memory.createTable();
-            console.log(mem1);
         },
         
+            
+            
+        // Skapar en wrapper för att kapsla in memoryspelet.
         createTable: function() {
             var wrapper = document.querySelector("#main");
             var div = document.createElement("div");
-            wrapper.appendChild(div);
             div.className = "memgame";
+            wrapper.appendChild(div);
+            
 
 
             // Skapar en tabell.
@@ -24,30 +32,46 @@
             table.className = "memtable";
             div.appendChild(table);
 
+
+            var MemoryID = 0;
+
             // Lägger till arrayen (med tillhörande bilder) till tabellen med hjälp av for-loopar.
-            for (var rows = 0; rows < 4; rows += 1) {
+            for (var r = 0; r < Memory.rows; r += 1) {
                 var tr = document.createElement("tr");
                 table.appendChild(tr);
                 
-                for (var cols = 0; cols < 4; cols += 1) {
-                    var td = document.createElement("td");
-                    tr.appendChild(td);
-                    
-                    var pic = document.createElement("img");
-                    pic.setAttribute("src", "pics/0.png");
-                    td.appendChild(pic);
+                for (var c = 0; c < Memory.cols; c += 1) {
+                    tr.appendChild(Memory.createMemorybrick(MemoryID));
+                    MemoryID += 1;
                 }
-            
             }
         },
         
-        start: function() {
-            
-            
-            
-            
+        
+        createMemorybrick: function(MemoryID) {
+                    var td = document.createElement("td");
+                    var link = document.createElement("a");
+                    var pic = document.createElement("img");
+                    
+                    link.setAttribute("href", "#");
+                    pic.setAttribute("src", "pics/0.png");
+                    pic.setAttribute("id", MemoryID);
+                    
+                    link.onclick = function() {
+                        Memory.memoryClick(MemoryID);
+                    };
+                    
+                    link.appendChild(pic);
+                    td.appendChild(link);
+                    return td;
+        },
+        
+        
+        memoryClick: function (MemoryID) {
+        var mempic = document.getElementById(MemoryID);
+        mempic.setAttribute("src", "pics/"+this.memoryArray[MemoryID]+".png");
         }
-            
+ 
 };
 
     // MessageBoard.init anropas när sidan är helt färdigladdad.
