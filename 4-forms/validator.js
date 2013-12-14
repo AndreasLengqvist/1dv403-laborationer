5239109db8f19444676d11cf28d2967459b636d4
Andreas Lengqvist <al223bn@student.lnu.se>
2013-12-14 09:25:49 -0500

    Testar onCorrect.

--- 4-forms/validator.js
+++ 4-forms/validator.js
@@ -4,18 +4,14 @@
 var Validator = {
 
 
-    fncount: 0,
+    IDcount: 0,
     lncount: 0,
 
     init: function() {
-        
-                    console.log("Inne i Validator")
-            
             
             var send = document.querySelector("#send");         // Sändknappens ID.
             var myform = document.querySelector("#myform");     // forumlärets ID.
                 
-                console.log("Inne i getFormInfo")
                 
                 // Hämtning av alla inputs id:n och deras values i formuläret.
                 var firstname = document.getElementById('fn');
@@ -27,41 +23,22 @@ var Validator = {
                 var pricetype = document.getElementById('pricetype');
                 var ptvalue = pricetype.value;
                 
+                
                 // Om förnamnet är tomt när det tappar fokus.
                 firstname.onblur = function() {
                     
                     // Kollar värdet av inputfältet.
                     var fnvalue = firstname.value;
+                    var fninput = document.querySelector(".fninput");
                     
                     // Om det inte är något värde i input.
                     if(fnvalue == ""){
-                        
-
-                        var fninput = document.querySelector(".fninput");
-                        fninput.style.background = "#FF7878";
-                        
-                        if(Validator.fncount == 0){
-                        
-                        // Skapar en span som fungerar som wrapper för att kunna sätta ett felmeddelande.
-                        var span = document.createElement("span");
-                        span.style.color = "#FF7878";
-                        span.id ="fnspan";
-                        span.appendChild(document.createTextNode(" Detta fält får inte lämnas blankt."));
-                        
-                        // Slänger in textnoden efter förälder lninput.
-                        fninput.parentNode.insertBefore(span, fninput.nextSibling);
-                        
-                        Validator.fncount +=1;
-                        console.log(Validator.fncount)
-
-                        }
+                        Validator.onEmpty(fninput);
                     }
                     
                     // Om det är ett värde i input.
                     else {
-                        Validator.fncount = 0;
-                        document.querySelector(".fninput").style.background = "#8CCD8C";
-                        document.querySelector("#fnspan").remove();
+                        Validator.onCorrect(fninput);
                     }
                 };
                 
@@ -72,47 +49,60 @@ var Validator = {
                     
                     // Kollar värdet av inputfältet.
                     var lnvalue = lastname.value;
+                    var lninput = document.querySelector(".lninput");
                     
                     // Om det inte är något värde i input.
                     if(lnvalue === ""){
-                        var lninput = document.querySelector(".lninput");
-                        lninput.style.background = "#FF7878";
+                        Validator.onEmpty(lninput);
                         
-                        
-                        if(Validator.lncount == 0){
-                        // Skapar en span som fungerar som wrapper för att kunna sätta ett felmeddelande.
-                        var span = document.createElement("span");
-                        span.id = "lnspan";
-                        span.style.color = "#FF7878";
-                        span.appendChild(document.createTextNode(" Detta fält får inte lämnas blankt."));
-                        
-                        // Slänger in textnoden efter förälder fninput.
-                        lninput.parentNode.insertBefore(span, lninput.nextSibling);
-                        
-                        Validator.lncount += 1;
                         }
-                    }
-                    
                     // Om det är ett värde i input.
                     else {
-                        Validator.lncount = 0;
-                        document.querySelector(".lninput").style.background = "#8CCD8C";
-                        document.querySelector("#lnspan").remove(document.querySelector("#lnspan"));
-                        
-                        return false;
+                        Validator.onCorrect(lninput);
                     }
+                    
                 };
                 
                 // Skickar iväg formuläret med alla inputs.
                 //myform.submit();
+                // Om förnamnet är tomt när det tappar fokus.
                 
+
             
             
     },
     
-    
-    getFormInfo: function() {
+    // Anropas när inputfältet är tomt.
+    onEmpty: function(inputID) {
         
+                        inputID.style.background = "#FF7878";
+                        console.log(Validator.IDcount)
+                        
+                        if(Validator.IDcount == 0){
+                        console.log(Validator.IDcount)
+                        // Skapar en span som fungerar som wrapper för att kunna sätta ett felmeddelande.
+                        var span = document.createElement("span");
+                        span.style.color = "#FF7878";
+                        span.id ="fnspan";
+                        span.appendChild(document.createTextNode(" Detta fält får inte lämnas blankt."));
+                        
+                        // Slänger in textnoden efter förälder lninput.
+                        inputID.parentNode.insertBefore(span, inputID.nextSibling);
+                        
+                        Validator.IDcount +=1;
+
+                        }
+    },
+    
+    // Anropas när inputfältet är correct inskrivet eller rättat.
+    onCorrect: function(inputID) {
+                    Validator.IDcount = 0;
+                    
+                    if(background !== "#8CCD8C"){
+                    document.querySelector("#fnspan").remove();
+                    }
+                    var background = inputID.style.background = "#8CCD8C";
+
     }
 
 
