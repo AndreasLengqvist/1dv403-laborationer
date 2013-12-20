fff32f84d445a26c8098250cd86c6c86958ac1d2
Andreas Lengqvist <al223bn@student.lnu.se>
2013-12-19 22:03:40 -0500

    Klar med inloggningen

--- 5-pwd/script/login.js
+++ 5-pwd/script/login.js
@@ -4,19 +4,18 @@ var Login = {
     
     buildLogin: function(){
         
-        
         var modalwindow = document.createElement("div");
         var loginwindow = document.createElement("div");
         var loginhead = document.createElement("label");
         var logininput = document.createElement("input");
         var loginbutton = document.createElement("button");
         
-        modalwindow.id = "modalwindow";
+        modalwindow.id = "modalwindow2";
         var main = document.querySelector("main");
         main.appendChild(modalwindow);
         loginbutton.appendChild(document.createTextNode("Logga in"));
         
-        loginwindow.id = "loginwindow";
+        loginwindow.id = "loginwindow2";
         loginhead.id = "loginhead";
         logininput.id = "logininput";
         loginbutton.id = "loginbutton";
@@ -31,9 +30,8 @@ var Login = {
         
         
         main.parentNode.insertBefore(loginwindow, main.nextSibling);
-        
-
-                logininput.focus();
+                        
+        logininput.focus();
 
         
         logininput.onkeypress = function(e){
@@ -42,14 +40,14 @@ var Login = {
                 }
             
             if (e.keyCode == 13 &! e.shiftKey) {
-                if(checklogin.value == 0){
+                if(logininput.value == 0){
                     logininput.className = logininput.className = " ";
                     logininput.className = logininput.className + " error";
                 }
                 else {
                     document.querySelector("#loginstatus").innerHTML = "Inloggad som "+logininput.value;
-                    modalwindow.remove();
-                    loginwindow.remove();
+                    modalwindow.id = "modalwindow";
+                    loginwindow.id = "loginwindow";
                 }
             }
         };
@@ -61,8 +59,8 @@ var Login = {
             }
             else {
                 document.querySelector("#loginstatus").innerHTML = "Inloggad som "+logininput.value;
-                modalwindow.remove();
-                loginwindow.remove();
+                modalwindow.id = "modalwindow";
+                loginwindow.id = "loginwindow";
             }
         };
     }
