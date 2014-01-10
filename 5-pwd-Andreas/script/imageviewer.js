8606257ea4f0e04215e43ee0efd96a2707af078c
Andreas Lengqvist <al223bn@student.lnu.se>
2014-01-10 10:00:35 -0500

    sparar

--- 5-pwd-Andreas/script/imageviewer.js
+++ 5-pwd-Andreas/script/imageviewer.js
@@ -27,8 +27,6 @@ var MYMAK = MYMAK || {};
                 
                 if (xhr.readyState == 4 && xhr.status == 200) {
                     ajaxobj = JSON.parse(xhr.responseText);
-                    console.log("Ajax-loaded");
-                    console.log(ajaxobj);
                     // Här ska loadern stoppas!
                     MYMAK.Imageviewer.prototype.objectSplitter(that.idname, ajaxobj);
                 }
@@ -43,30 +41,65 @@ var MYMAK = MYMAK || {};
             // Splitfunktion som loopar igenom objekten och splittar upp dem.
             MYMAK.Imageviewer.prototype.objectSplitter = function(name, objects){
                 
+                var widthArray = [];
+                var heightArray = [];
                 var pictures = [];
                 pictures = objects;
                 
                 for (var i = 0; i < pictures.length; i += 1) {
+                                    
+                    widthArray.push(pictures[i].thumbWidth)
+                    heightArray.push(pictures[i].thumbHeight)
+                    console.log(pictures[i].thumbHeight)
+                    
+                    MYMAK.Imageviewer.prototype.adjustMaker(heightArray, widthArray);
                     MYMAK.Imageviewer.prototype.pictureMaker(name, objects, pictures, i);
                 }
             };
             
             
-            // Bildredigerarfunktion som redigerar och visar upp bilderna i applikationen.
+            // Bildredigerarfunktion som redigerar och visar upp bilden i applikationen. (Ligger i innersluten i loopen från funktionen objectsSplitter)
             MYMAK.Imageviewer.prototype.pictureMaker = function(name, objects, pictures, i){
-                console.log("inne i tredje steget");
 
                 var windowdiv = document.querySelector("#"+name);
                 var imagediv = document.createElement("div");
-                imagediv.id = "Imageviewer";
+                var imagelink = document.createElement("a");
+                var image = document.createElement("img");
                 
                 
-                console.log(name)
                 
+                imagediv.className = "imagewrapper";
+                imagelink.className = "imagelink";
+                
+                    // Sätter tumnagelbildsattributen.
+                    image.setAttribute("src", pictures[i].thumbURL);
+                    image.setAttribute("height", pictures[i].thumbHeight);
+                    image.setAttribute("width", pictures[i].thumbWidth);
+                    
+                imagelink.appendChild(image);
+                imagediv.appendChild(imagelink);
                 windowdiv.appendChild(imagediv);
+                
+                
+                
+                
+                
                 
             };
 
+
+            // Sparar ner alla bredder och höjder på bilderna, jämför och sätter sedan den bredaste som alla andra bilders wrappers tar efter.
+            MYMAK.Imageviewer.prototype.adjustMaker = function(heightArray, widthArray){
+                
+                
+                var maxwidth = Math.max(widthArray);
+                var maxheight = Math.max(heightArray);
+                
+                console.log(maxheight)
+                console.log(maxwidth)
+                
+
+            };
             
             // XHR-requestfunktion.
             MYMAK.Imageviewer.prototype.getXHR = function(){
